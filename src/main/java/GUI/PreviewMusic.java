a918182fd577195643b8ae8998e484984adb9393
Long Lin <lonlin@my.yorku.ca>
2022-02-06 18:23:16 -0500

    Make window for play music notes function(Play and Pause)
    Created a new class NotePlayer in src/main/java/GUI, modified playHandle
    method, and added a OpenWindow method in PreviewMusic class

--- src/main/java/GUI/PreviewMusic.java
+++ src/main/java/GUI/PreviewMusic.java
@@ -3,6 +3,8 @@ package GUI;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import GUI.draw.DrawClef;
 import GUI.draw.DrawMusicLines;
@@ -10,6 +12,8 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
@@ -28,6 +32,8 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Window;
 import javafx.stage.Stage;
 import models.measure.attributes.Clef;
 
@@ -35,10 +41,12 @@ import java.util.List;
 import parser.Parser;
 import converter.Converter;
 
+
 public class PreviewMusic extends Application{
    
 	private MainViewController mvc;
 	@FXML private Pane pane;
+	public Window convertWindow;
     
 
 
@@ -161,10 +169,34 @@ public class PreviewMusic extends Application{
     	System.out.println("Print button is clicked");
     	
     }
+	@FXML
     public void playHandle() {
-    	System.out.println("Play button is clicked");
+    	Parent root;
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("GUI/tabPlayer.fxml"));
+			root = loader.load();
+			NotePlayer controller = loader.getController();
+			controller.setMainViewController(this);
+			
+			convertWindow = this.openNewWindow(root, "Play Music Notes");
+		} catch (IOException e) {
+			Logger logger = Logger.getLogger(getClass().getName());
+			logger.log(Level.SEVERE, "Failed to create new Window.", e);
+		}
     }
-    public void handleGotoMeasure() {}
+    private Window openNewWindow(Parent root, String windowName) {
+    	Stage stage = new Stage();
+		stage.setTitle(windowName);
+		stage.initModality(Modality.APPLICATION_MODAL);
+		stage.initOwner(MainApp.STAGE);
+		stage.setResizable(false);
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		return scene.getWindow();
+	}
+
+	public void handleGotoMeasure() {}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
