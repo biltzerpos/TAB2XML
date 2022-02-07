9c94b588365bc3c9665efcb8119a4c64f7c9ccb2
inasi022 <inasi022@my.yorku.ca>
2022-02-06 22:57:17 -0500

    Added print function to print music sheet

--- src/main/java/GUI/PreviewMusic.java
+++ src/main/java/GUI/PreviewMusic.java
@@ -4,7 +4,14 @@ import java.io.File;
 
 
 import javafx.application.Application;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
+import javafx.print.PageLayout;
+import javafx.print.PageOrientation;
+import javafx.print.Paper;
+import javafx.print.Printer;
+import javafx.print.PrinterJob;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -24,9 +31,30 @@ public class PreviewMusic {
     	mvc = mvcInput;
     }
     
-    public void printHandle() {
-    	System.out.println("Print button is clicked");
+    @FXML
+    private Button printButton;
+    
+    final BooleanProperty printButtonPressed = new SimpleBooleanProperty(false);
+    
+    @FXML
+    public <printButtonPressed> void printHandle() {
+    	printButton.setOnAction( aEvent -> {
+    	    printButtonPressed.set(true);
+    	});
+    	
+    	Printer printer = Printer.getDefaultPrinter();
+        PageLayout layout = printer.createPageLayout(Paper.A4, PageOrientation.PORTRAIT, Printer.MarginType.HARDWARE_MINIMUM);
+        PrinterJob printSheet = PrinterJob.createPrinterJob();
+       
+        if (printSheet != null && printSheet.showPrintDialog(pane.getScene().getWindow())) {
+          boolean printed = printSheet.printPage(layout, pane);
+          if (printed) {
+        	  printSheet.endJob();
+          }
+        }
     }
+
+
     public void playHandle() {
     	System.out.println("Play button is clicked");
     }
