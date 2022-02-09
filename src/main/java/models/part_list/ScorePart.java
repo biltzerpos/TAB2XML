3bf70786c604fff686a071f2a60e2670d969d276
khafaji-ahmed <ahmed03@my.yorku.ca>
2022-02-09 13:03:53 -0500

    build.gradle fixes

--- src/main/java/models/part_list/ScorePart.java
+++ src/main/java/models/part_list/ScorePart.java
@@ -1,7 +1,7 @@
 package models.part_list;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+//import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 import java.util.List;
