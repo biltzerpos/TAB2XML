a220bc59bcaedab0f37797312c36a29b70b9403b
CCSClassBattlecruiser <623161603@qq.com>
2022-02-09 16:03:02 -0500

    add comments

---
+++ src/main/java/visualizer/visualExporter.java
@@ -0,0 +1,17 @@
+package visualizer;
+
+import javafx.scene.canvas.Canvas;
+/**
+ * visualExporter will handle draw calls from visualizer
+ * for each draw call, visualExporter will draw in the template file.
+ * after draw call is compete, template file will display in the canvas
+ *
+ *
+ * @author Kuimou
+ * */
+public class visualExporter {
+	public visualExporter(){
+
+	}
+
+}
--- src/main/java/visualizer/visualizer.java
+++ src/main/java/visualizer/visualizer.java
@@ -11,36 +11,58 @@ import models.measure.attributes.Attributes;
 import models.measure.barline.BarLine;
 import models.measure.note.Note;
 import models.part_list.PartList;
-import java.util.List;
+import org.jfugue.integration.MusicXmlParserListener;
 
+import java.util.List;
+/**
+ * This Class is use for visualize musicXML file.
+ *
+ * @author Kuimou
+ * */
 public class visualizer {
 	private ScorePartwise score;
 	public visualizer(Score score) throws TXMLException {
 		this.score = score.getModel();
 	}
-	public void draw(Canvas canvas)  {
+	/**
+	 * This method is going to draw musicXML in visualExporter.
+	 * visualExporter will create visualElements in both canvas and a PDF file.
+	 *
+	 * @param canvas
+	 * canvas in the "preview page"
+	 * */
+	public void draw(Canvas canvas) {
+		// PartList is the instrument information of each Part
+		// Parts is collection of part
 		drawPartList(score.getPartList());
 		drawParts(score.getParts());
 	}
 	public void drawParts(List<Part> parts){
+		//draw each part
 		for (Part part:parts){
 			drawPart(part);
 		}
 	}
 	public void drawPart(Part part){
+		//draw each measures
+		// there will be additional metadata in the Part that we have to draw
 		List<Measure> measures = part.getMeasures();
+		//draw each measure
 		for (Measure measure:measures){
 			drawMeasure(measure);
 		}
 	}
 	public void drawPartList(PartList partList){
-
+		// some data to draw
 	}
 
 	public void drawMeasure(Measure measure){
+		// attribute contain metadatas for whole measure
 		drawAttributes(measure.getAttributes());
+		// draw Notes
 		drawNotes(measure.getNotesBeforeBackup());
 		drawNotes(measure.getNotesAfterBackup());
+		// draw Barlines
 		drawBarlines(measure.getBarlines());
 	}
 	public void drawAttributes(Attributes attributes){
