1e4a3866e901b702817dfedc074fd7d75f010da5
ElmiraOn <elmiraonagh@yahoo.com>
2022-02-18 01:30:41 -0500

    updated draw() to set the background of fret notes to white.
    the draw() of DrawNote class was changed to DrawFret(). Additionally a
    3D cube was used and set to white to be used as background for the
    printed frets and solve the problem of overlapping background on the
    note Texts.

--- src/main/java/GUI/draw/DrawNote.java
+++ src/main/java/GUI/draw/DrawNote.java
@@ -3,12 +3,12 @@ package GUI.draw;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import models.measure.note.Note;
 
 public class DrawNote {
 	
@@ -25,19 +25,28 @@ public class DrawNote {
 		this.pane = pane;
 	}
 	
-	public void draw() {
-		/*Rectangle textBack = new Rectangle(getStartX() -3, getStartY() - 12, 15, 15);
-		textBack.setFill(Color.WHITE);
-		pane.getChildren().add(textBack);*/
+	/*
+	 * this method draws note based on given Fret of guitar
+	 * */
+	public void drawFret() {
+		// for the background of each note a cube is used
+		Box box = new Box(13, 13, 1); 
+		box.setTranslateX(getStartX()+3);
+		box.setTranslateY(getStartY()-3);
+		box.setTranslateZ(-15);
+		PhongMaterial boxColor = new PhongMaterial();
+		boxColor.setSpecularColor(Color.WHITE);
+		boxColor.setDiffuseColor(Color.WHITE);
+		boxColor.setSpecularPower(0);
+		box.setMaterial(boxColor);
+		pane.getChildren().add(box);
 		
+		//the actual notes based on fret value of guitar
 		Text text = new Text(getStartX(), getStartY(), Integer.toString(this.fret));
     	text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 10));
+    	text.toFront();
     	pane.getChildren().add(text);
 	}
-	
-
-
-	
 
 	public double getStartX() {
 		return startX;
