c310ac5bde628d0356f8ebfbe1da341b6ae7b944
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-02-19 17:21:47 -0500

    Added invisible lines above sheet music lines for drums.
    Above every drumset sheet music line there is an invisible line. This
    makes it easier to place notes between lines. Also made the top line
    invisible so that notes can be placed higher above the sheet music
    lines. Also added documentation for some of the methods.

--- src/main/java/GUI/draw/DrawDrumsetMusicLines.java
+++ src/main/java/GUI/draw/DrawDrumsetMusicLines.java
@@ -23,28 +23,57 @@ public class DrawDrumsetMusicLines extends Node {
 		this.musicLineList = new ArrayList<Line>();
 	}
 
+	/**
+	 * Draws the sheet music lines and adds invisible lines for placing notes above and below lines.
+	 *
+	 * @param x The x-coordinate of the music lines
+	 * @param y The y-coordinate of the music lines
+	 */
 	public void draw(double x, double y) {
-		for (int i = 0; i < 6; i++) {
-			Line line = new Line();
+		// Make the first line invisible (the third parameter is visible = false).
+		// We do this so that we can draw notes on the top line without the user seeing the line
+		// (because the user should only see 5 lines).
+		this.addLine(x, y, false);
 
-			// Make the first line white.
-			// We do this so that we can draw notes on the top line without the user seeing the line
-			// (because the user should only see 5 lines instead of 6).
-			if (i == 0) {
-				line.setStyle("-fx-stroke: white");
-			}
+		y += 5;
 
-			line.setStartX(x);
-			line.setStartY(y);
-			line.setEndX(x + this.length);
-			line.setEndY(y);
+		for (int i = 1; i < 6; i++) {
+			// Above every visible line, draw an invisible line.
+			// We do this so that we can place notes in between lines easily.
+			this.addLine(x, y, false);
 
-			pane.getChildren().add(line);
+        	y += 5;
 
-        	y+=10;
+        	// Draw the visible line
+			this.addLine(x, y, true);
 
-        	musicLineList.add(line);
+        	y += 5;
     	}
+	}
+
+	/**
+	 * Draws an individual line.
+	 *
+	 * @param x       The x-coordinate of the line
+	 * @param y       The y-coordinate of the line
+	 * @param visible The visibility of the line. If set to false, the line will not be drawn to the screen.
+	 */
+	private void addLine(double x, double y, boolean visible) {
+		Line line = new Line();
+
+		line.setStartX(x);
+		line.setStartY(y);
+		line.setEndX(x + this.length);
+		line.setEndY(y);
+
+		// Only draw the line if visible is true.
+		// Otherwise, skip this and just add it to musicLineList so that it can
+		// be used to place notes between lines.
+		if (visible) {
+			pane.getChildren().add(line);
+		}
+
+    	musicLineList.add(line);
 	}
 
 	public List<Line> getMusicLineList() {
