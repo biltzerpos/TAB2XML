ee3b4d0f4e20e8b60796d4ff528d9db72821aef4
ElmiraOn <elmiraonagh@yahoo.com>
2022-02-19 22:16:51 -0500

    Fixed the overflow problem for the long tablatures

--- src/main/java/GUI/draw/Draw44Beat.java
+++ src/main/java/GUI/draw/Draw44Beat.java
@@ -1,6 +1,9 @@
 package GUI.draw;
 
 import javafx.fxml.FXML;
+import javafx.scene.effect.Blend;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.ColorInput;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -26,21 +29,14 @@ public class Draw44Beat {
 	public void draw() {
 		String label = "44";
 		for(int i = 0; i < label.length(); i++) {
-			Box box = new Box(15, 15, 1); 
-			box.setTranslateX(x+7);
-			box.setTranslateY(y-7);
-			box.setTranslateZ(-15);
-			PhongMaterial boxColor = new PhongMaterial();
-			boxColor.setSpecularColor(Color.WHITE);
-			boxColor.setDiffuseColor(Color.WHITE);
-			boxColor.setSpecularPower(0);
-			box.setMaterial(boxColor);
-			pane.getChildren().add(box);
 			
 			char c = label.charAt(i);
 			String s = Character.toString(c);
 			Text text = new Text(this.x, this.y,  s);
 			text.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, FontPosture.REGULAR, 20));
+			
+			
+			
 			pane.getChildren().add(text);
 			y += 20;
 			
