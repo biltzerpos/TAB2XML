bfad20aedd73ff18d4fda8010de91e0d52ebeb92
inasi022 <inasi022@my.yorku.ca>
2022-02-19 22:18:46 -0500

    Added DrawClef test

--- src/test/java/GUI/draw/DrawClefTest.java
+++ src/test/java/GUI/draw/DrawClefTest.java
@@ -4,18 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import javafx.scene.layout.Pane;
+import models.measure.attributes.Clef;
+
 class DrawClefTest {
 
+	
 	@Test
-	void test() {
-		DrawClef clef = new DrawClef();
-		clef.setX(3);
-		clef.setY(40);
+	void testPane() {
+		Clef clef=new Clef("clef",3);
+		Pane pane = new Pane();
 		
-		assertEquals(3, clef.getX());
-		assertEquals(4, clef.getY());
+		DrawClef clef2 = new DrawClef(pane, clef, 30, 30);
+		
+		assertEquals(30.0, clef2.getX());
+		assertEquals(30.0, clef2.getY());
+		assertEquals(pane, clef2.getPane());
+		assertEquals(clef, clef2.getClef());
 	}
-	
-	
 
 }
