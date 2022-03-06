80b17e58cc85c62a9a090edafed2c026c332661e
ElmiraOn <elmiraonagh@yahoo.com>
2022-03-06 01:08:08 -0500

    updated and cleaned MLine.java

--- src/main/java/GUI/draw/MLine.java
+++ src/main/java/GUI/draw/MLine.java
@@ -4,28 +4,33 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
 
-public class MLine extends Line{
-	
-	@FXML private Pane pane;
+//Creates a Line with tag between 1-6
+public class MLine extends Line {
+
+	@FXML
+	private Pane pane;
 	private int lineTag;
 	private double startY;
 	private double startX;
-	private double endX; 
-	private double endY; 
-	
-	public MLine() {}
-	public MLine(Pane pane, double startX, double startY, double endX, double endY, int tag) {
-    	this.pane = pane;
-		this.startX = startX; 
-    	this.startY = startY; 
-    	this.endX = endX;
-    	this.endY = endY; 
-    	this.lineTag = tag;
-    	createLines();
-        
+	private double endX;
+	private double endY;
+
+	public MLine() {
 	}
-	
-	public void createLines() {
+
+	public MLine(Pane pane, double startX, double startY, double endX, double endY, int tag) {
+		this.pane = pane;
+		this.startX = startX;
+		this.startY = startY;
+		this.endX = endX;
+		this.endY = endY;
+		this.lineTag = tag;
+		createLines();
+
+	}
+
+	// Method to draw line on pane
+	private void createLines() {
 		Line line = new Line();
 		line.setStartX(startX);
 		line.setStartY(startY);
@@ -33,16 +38,32 @@ public class MLine extends Line{
 		line.setEndY(endY);
 		pane.getChildren().add(line);
 	}
-	
+
+	// returns the x coordinates of a line with specific tag
+	public double getStartX(int tag) {
+		return this.startX;
+	}
+
+	// returns the Y coordnicate of a line with specific tag
+	public double getStartY(int tag) {
+		return this.startY;
+	}
+
+	// Getter and setters
+	public Pane getPane() {
+		return pane;
+	}
+
+	public void setPane(Pane pane) {
+		this.pane = pane;
+	}
+
 	public int getLineTag() {
 		return lineTag;
 	}
-	public double getStartX (int tag) {
-		return this.startX;
+
+	public void setLineTag(int lineTag) {
+		this.lineTag = lineTag;
 	}
-	public double getStartY (int tag) {
-		return this.startY;
-	}
-	
 
 }
