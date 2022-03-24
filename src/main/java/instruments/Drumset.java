656b8a6f7fde21c43518cfbd21b5094be5ac39f4
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-03-24 15:39:31 -0400

    Added invisible line below staff and ignored all rest notes.
    Added an extra line below the staff (invisible) so that notes can be
    added below the staff line. Also ignored rest notes so that parabola.txt
    can run without errors (rest notes are not drawn yet).

--- src/main/java/instruments/Drumset.java
+++ src/main/java/instruments/Drumset.java
@@ -98,50 +98,52 @@ public class Drumset {
 			// otherwise, we add 25 to draw it on its own column.
 			xPositionNote = currentNote.getChord() == null ? this.x + this.spacing / 2 : this.x - this.spacing / 2;
 
-			// y-position of note
-			// Get the y-position of the note based on its octave and step
-			// and the position of the music lines it will be drawn on.
-			yPositionNote = d.getYPositionFromOctaveAndStep(currentNote.getUnpitched().getDisplayOctave(),
-					currentNote.getUnpitched().getDisplayStep());
-
-			// Set note drawer for the current note
-			noteDrawer = new DrawDrumsetNote(this.pane, currentNote, yPositionMeasure, this.spacing, xPositionNote, yPositionNote);
-
-			if (currentNote.getChord() != null || currentNote.getGrace() != null) {
-				// If the note is a chord or grace note, then the beam will already have been
-				// drawn, so just draw the note
-				noteDrawer.draw();
-			} else if (currentNote.getType().equals("eighth") && nextNote != null) {
-				// If the current note is an eighth note and the next note is not null,
-				// draw a beam for the next note to be connected to.
-				// The beam must be a single beam because it is an eighth note.
-				// (Note that the next note being null also can mean that the rest of the notes
-				// in the group are chords.)
-				noteDrawer.draw();
-				noteDrawer.drawSingleBeam();
-			} else if (currentNote.getType().equals("16th") && nextNote != null) {
-				// If the current note is a 16th note and the next note is not null and is a
-				// 16th note,
-				// draw a beam for the next note to be connected to.
-				// (Note that the next note being null also can mean that the rest of the notes
-				// in the group are chords.)
-				noteDrawer.draw();
-
-				if (nextNote.getGrace() != null && i + 2 < notes.size()) {
-					nextNote = notes.get(i + 2);
-				}
-
-				if (nextNote.getType().equals("eighth")) {
-					// Draw a single beam because the next note is an eighth note
+			if (currentNote.getRest() == null) {
+				// y-position of note
+				// Get the y-position of the note based on its octave and step
+				// and the position of the music lines it will be drawn on.
+				yPositionNote = d.getYPositionFromOctaveAndStep(currentNote.getUnpitched().getDisplayOctave(),
+						currentNote.getUnpitched().getDisplayStep());
+	
+				// Set note drawer for the current note
+				noteDrawer = new DrawDrumsetNote(this.pane, currentNote, yPositionMeasure, this.spacing, xPositionNote, yPositionNote);
+	
+				if (currentNote.getChord() != null || currentNote.getGrace() != null) {
+					// If the note is a chord or grace note, then the beam will already have been
+					// drawn, so just draw the note
+					noteDrawer.draw();
+				} else if (currentNote.getType().equals("eighth") && nextNote != null) {
+					// If the current note is an eighth note and the next note is not null,
+					// draw a beam for the next note to be connected to.
+					// The beam must be a single beam because it is an eighth note.
+					// (Note that the next note being null also can mean that the rest of the notes
+					// in the group are chords.)
+					noteDrawer.draw();
 					noteDrawer.drawSingleBeam();
-				} else if (nextNote.getType().equals("16th")) {
-					// Draw a double beam because the current note and next note are both 16th notes
-					noteDrawer.drawDoubleBeam();
+				} else if (currentNote.getType().equals("16th") && nextNote != null) {
+					// If the current note is a 16th note and the next note is not null and is a
+					// 16th note,
+					// draw a beam for the next note to be connected to.
+					// (Note that the next note being null also can mean that the rest of the notes
+					// in the group are chords.)
+					noteDrawer.draw();
+	
+					if (nextNote.getGrace() != null && i + 2 < notes.size()) {
+						nextNote = notes.get(i + 2);
+					}
+	
+					if (nextNote.getType().equals("eighth")) {
+						// Draw a single beam because the next note is an eighth note
+						noteDrawer.drawSingleBeam();
+					} else if (nextNote.getType().equals("16th")) {
+						// Draw a double beam because the current note and next note are both 16th notes
+						noteDrawer.drawDoubleBeam();
+					}
+				} else {
+					// This probably means this is the last note in the group, just draw the note
+					// (if it is beamed, then the beam will already have been drawn).
+					noteDrawer.draw();
 				}
-			} else {
-				// This probably means this is the last note in the group, just draw the note
-				// (if it is beamed, then the beam will already have been drawn).
-				noteDrawer.draw();
 			}
 
 			// If the current note is not a chord, increment x position
@@ -184,23 +186,25 @@ public class Drumset {
 			// otherwise, we add 25 to draw it on its own column.
 			xPositionNote = currentNote.getChord() == null ? this.x + this.spacing / 2 : this.x - this.spacing / 2;
 
-			// y-position of note
-			// Get the y-position of the note based on its octave and step
-			// and the position of the music lines it will be drawn on.
-			yPositionNote = d.getYPositionFromOctaveAndStep(currentNote.getUnpitched().getDisplayOctave(),
-					currentNote.getUnpitched().getDisplayStep());
-
-			// Set note drawer for the current note
-			noteDrawer = new DrawDrumsetNote(this.pane, currentNote, yPositionMeasure, this.spacing, xPositionNote, yPositionNote);
-
-			// Draw the note
-			noteDrawer.draw();
-
-			// Draw the flag only if the current note is not a chord
-			// (if the current note is a chord, then the flag will have already
-			// been drawn by the previous note).
-			if (currentNote.getChord() == null) {
-				noteDrawer.drawFlag();
+			if (currentNote.getRest() == null) {
+				// y-position of note
+				// Get the y-position of the note based on its octave and step
+				// and the position of the music lines it will be drawn on.
+				yPositionNote = d.getYPositionFromOctaveAndStep(currentNote.getUnpitched().getDisplayOctave(),
+						currentNote.getUnpitched().getDisplayStep());
+	
+				// Set note drawer for the current note
+				noteDrawer = new DrawDrumsetNote(this.pane, currentNote, yPositionMeasure, this.spacing, xPositionNote, yPositionNote);
+	
+				// Draw the note
+				noteDrawer.draw();
+	
+				// Draw the flag only if the current note is not a chord
+				// (if the current note is a chord, then the flag will have already
+				// been drawn by the previous note).
+				if (currentNote.getChord() == null) {
+					noteDrawer.drawFlag();
+				}
 			}
 
 			// If the current note is a chord, increment x position
@@ -231,6 +235,10 @@ public class Drumset {
 		// The sum of the duration of the current group of notes
 		int durationSum = 0;
 
+		if (noteList == null) {
+			return;
+		}
+
 		while (i < noteList.size()) {
 			// Add the first note of the measure to the group
 			currentNote = noteList.get(i);
@@ -248,7 +256,7 @@ public class Drumset {
 			// When durationSum % divisions == 0, that means we stopped at a beat (quarter
 			// note).
 			// Otherwise, keep iterating until we find a beat.
-			while (durationSum != 0 && durationSum % divisions != 0) {
+			while (durationSum != 0 && durationSum % divisions != 0 && i < noteList.size()) {
 				currentNote = noteList.get(i);
 				group.add(currentNote);
 				// Only add to the duration sum if it not a chord
@@ -338,6 +346,10 @@ public class Drumset {
 	private double getMeasureLength(Measure measure) {
 		List<Note> notes = measure.getNotesBeforeBackup();
 		int length = 0;
+
+		if (notes == null) {
+			return 0;
+		}
 
 		for (Note note : notes) {
 			if (note.getChord() == null && note.getGrace() == null) {
