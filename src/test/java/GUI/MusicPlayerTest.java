7c04f56ed3703e093513f601b5bde11da0ef52ce
Long Lin <lonlin@my.yorku.ca>
2022-04-10 18:58:42 -0400

    Added MusicPlayer test case Bass

--- src/test/java/GUI/MusicPlayerTest.java
+++ src/test/java/GUI/MusicPlayerTest.java
@@ -64,6 +64,37 @@ BD|o-------o-------|o-------o-------|
 		//Drum: V9  [Crash_Cymbal_1]i+[Bass_Drum]i [Closed_Hi_Hat]i [Closed_Hi_Hat]i+[Acoustic_Snare]i [Closed_Hi_Hat]i [Closed_Hi_Hat]i+[Bass_Drum]i [Closed_Hi_Hat]i [Closed_Hi_Hat]i+[Acoustic_Snare]i [Closed_Hi_Hat]i [Acoustic_Snare]s+[Bass_Drum]s [Acoustic_Snare]s [Acoustic_Snare]s [Acoustic_Snare]s [Lo_Mid_Tom]s [Lo_Mid_Tom]s [Lo_Tom]s [Lo_Tom]s [Crash_Cymbal_1]h+[Bass_Drum]h
 	}
 	
+	private Pattern getBassString(String input) throws TXMLException {
 
-	//Bass: T180 V0 I[Acoustic_Bass]  B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q F#2q F#2q F#2i C#2i F#1q A1q C#2q F#2q F2q E2i B1i E1q G1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q
+		Score score = new Score(input);
+		ScorePartwise scorePartwise = score.getModel();
+		MusicPlayer sequence = new MusicPlayer(scorePartwise);
+		sequence.getBassString();
+		return sequence.getPattern();
+	}
+	@Test
+	void testBass() throws TXMLException {
+		String input = """
+Intro
+
+
+   7/4
+  REPEAT 8x
+G---4-----------|
+D----4----------|
+A-2---2-----2-5-|
+E-------2-5-----|
+
+G---------------|-----------------------------|--4-----------|
+D-4-4-4-------4-|-3---2-----------------------|---4----------|
+A------4----4---|-------2---------0---2---5---|2---2-----2-5-|
+E-------2-5-----|---------0---3---------------|------2-5-----|
+
+				""";
+
+		Pattern bass = getBassString(input);
+
+		assertEquals(bass.toString(), "T180 V0 I[Acoustic_Bass]  B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q F#2q F#2q F#2i C#2i F#1q A1q C#2q F#2q F2q E2i B1i E1q G1q A1q B1q D2q B1q B2i F#2i B1q F#1q A1q B1q D2q");
+	}
+	
 }
