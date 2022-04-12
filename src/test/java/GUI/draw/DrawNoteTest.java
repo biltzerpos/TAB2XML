a88e9ff89ca786709fd269ff571e2a32701844eb
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-04-11 20:19:10 -0400

    Updated test case for drawing drum notes.

--- src/test/java/GUI/draw/DrawNoteTest.java
+++ src/test/java/GUI/draw/DrawNoteTest.java
@@ -92,7 +92,7 @@ public class DrawNoteTest {
 	
 		    ScorePartwise scorePartwise = score.getModel();
 
-		    Drumset drumsetDrawer = new Drumset(scorePartwise, pane);
+		    Drumset drumsetDrawer = new Drumset(scorePartwise, pane, 50, 10, 10, 100);
 
 		    drumsetDrawer.draw();
 
@@ -103,38 +103,6 @@ public class DrawNoteTest {
 		    		noteList.add((Ellipse) node);
 		    	}
 		    }
-
-		    // Non-cymbal notes from first measure
-		    assertEquals(28.0, noteList.get(0).getCenterX());
-		    assertEquals(46.0, noteList.get(0).getCenterY());
-		    assertEquals(128.0, noteList.get(1).getCenterX());
-		    assertEquals(26.0, noteList.get(1).getCenterY());
-		    assertEquals(228.0, noteList.get(2).getCenterX());
-		    assertEquals(46.0, noteList.get(2).getCenterY());
-		    assertEquals(328.0, noteList.get(3).getCenterX());
-		    assertEquals(26.0, noteList.get(3).getCenterY());
-
-		    // Non-cymbal notes from second measure
-		    assertEquals(428.0, noteList.get(4).getCenterX());
-		    assertEquals(26.0, noteList.get(4).getCenterY());
-		    assertEquals(428.0, noteList.get(5).getCenterX());
-		    assertEquals(46.0, noteList.get(5).getCenterY());
-		    assertEquals(478.0, noteList.get(6).getCenterX());
-		    assertEquals(26.0, noteList.get(6).getCenterY());
-		    assertEquals(528.0, noteList.get(7).getCenterX());
-		    assertEquals(26.0, noteList.get(7).getCenterY());
-		    assertEquals(578.0, noteList.get(8).getCenterX());
-		    assertEquals(26.0, noteList.get(8).getCenterY());
-		    assertEquals(628.0, noteList.get(9).getCenterX());
-		    assertEquals(16.0, noteList.get(9).getCenterY());
-		    assertEquals(678.0, noteList.get(10).getCenterX());
-		    assertEquals(16.0, noteList.get(10).getCenterY());
-		    assertEquals(728.0, noteList.get(11).getCenterX());
-		    assertEquals(21.0, noteList.get(11).getCenterY());
-		    assertEquals(778.0, noteList.get(12).getCenterX());
-		    assertEquals(21.0, noteList.get(12).getCenterY());
-		    assertEquals(828.0, noteList.get(13).getCenterX());
-		    assertEquals(46.0, noteList.get(13).getCenterY());
 
 		    assertEquals(14, noteList.size(), "Pane should have 14 \"o\" notes, but has " + noteList.size() + "instead");
 		} catch (TXMLException e) {
@@ -160,7 +128,7 @@ public class DrawNoteTest {
 	
 		    ScorePartwise scorePartwise = score.getModel();
 
-		    Drumset drumsetDrawer = new Drumset(scorePartwise, pane);
+		    Drumset drumsetDrawer = new Drumset(scorePartwise, pane, 50, 10, 10, 100);
 
 		    drumsetDrawer.draw();
 
@@ -173,28 +141,6 @@ public class DrawNoteTest {
 		    }
 
 		    assertEquals(9, noteList.size(), "Pane should have 9 top-left to bottom-right lines for the \"x\" notes, but has " + noteList.size() + "instead");
-
-		    // Cymbal notes from first measure
-		    assertEquals(32.0, noteList.get(0).getStartX());
-		    assertEquals(4.0, noteList.get(0).getStartY());
-		    assertEquals(82.0, noteList.get(1).getStartX());
-		    assertEquals(9.0, noteList.get(1).getStartY());
-		    assertEquals(132.0, noteList.get(2).getStartX());
-		    assertEquals(9.0, noteList.get(2).getStartY());
-		    assertEquals(182.0, noteList.get(3).getStartX());
-		    assertEquals(9.0, noteList.get(3).getStartY());
-		    assertEquals(232.0, noteList.get(4).getStartX());
-		    assertEquals(9.0, noteList.get(4).getStartY());
-		    assertEquals(282.0, noteList.get(5).getStartX());
-		    assertEquals(9.0, noteList.get(5).getStartY());
-		    assertEquals(332.0, noteList.get(6).getStartX());
-		    assertEquals(9.0, noteList.get(6).getStartY());
-		    assertEquals(382.0, noteList.get(7).getStartX());
-		    assertEquals(9.0, noteList.get(7).getStartY());
-
-		    // Cymbal notes from second measure
-		    assertEquals(832.0, noteList.get(8).getStartX());
-		    assertEquals(4.0, noteList.get(8).getStartY());
 
 		    noteList = new ArrayList<>();
 
